cd11e3ef94a917f470e6e12b22676c61a97a5860
bb <oleh.kvsk@gmail.com>
2022-11-15 18:55:02 +0200

    test morph

---
+++ modules/swiper/Swiper.tsx
@@ -0,0 +1,33 @@
+import { Swiper as BaseSwiper, SwiperSlide } from "swiper/react";
+import universe from "./images/universe.png";
+import satellite from "./images/satellite.png";
+import people from "./images/people.png";
+import Image from "next/image";
+import styles from "./Swiper.module.css";
+import "swiper/css/bundle";
+import "swiper/css/effect-coverflow";
+import { Mousewheel } from "swiper";
+
+const images = [universe, satellite, people];
+
+interface Props {
+  onActiveIndexChange: (index: number) => void;
+}
+
+export const Swiper = ({ onActiveIndexChange }: Props) => {
+  return (
+    <BaseSwiper
+      direction="vertical"
+      modules={[Mousewheel]}
+      mousewheel
+      className={styles.swiper}
+      onActiveIndexChange={(e) => onActiveIndexChange(e.activeIndex)}
+    >
+      {images.map((src, index) => (
+        <SwiperSlide key={index} className={styles.slide}>
+          <Image src={src} alt="universe" fill className={styles.image} />
+        </SwiperSlide>
+      ))}
+    </BaseSwiper>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div>hello</div>;
-}
+import { Swiper } from "modules/swiper";
+import { useState } from "react";
+import { MorphingEntity } from "../modules/MorphingEntity";
+
+const Home = () => {
+  const [activeSlide, setActiveSlide] = useState(0);
+  console.log(activeSlide);
+  return (
+    <div>
+      <Swiper onActiveIndexChange={setActiveSlide} />
+      <MorphingEntity state={activeSlide} />
+    </div>
+  );
+};
+
+export default Home;
